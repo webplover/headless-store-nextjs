94a5bb1503d8a734498e6c4d4befb324ca1920fa
Kazim <kazimshah39@gmail.com>
2022-07-17 19:17:22 +0500

    products grid added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const allowedWPimageDomain = new URL(process.env.NEXT_PUBLIC_WP_URL).hostname;
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [allowedWPimageDomain],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import Head from "next/head";
+import Link from "next/link";
 import Image from "next/image";
 // import styles from "../styles/Home.module.css";
 
@@ -7,13 +7,52 @@ export default function Home(props) {
   const { data } = props;
   console.log(data.data);
   return (
-    <div class="flex flex-wrap -mx-1 overflow-hidden">
+    <div className="flex flex-wrap overflow-hidden p-8 container mx-auto">
       {data.data.map((product, index) => (
         <div
-          className="my-1 px-1 w-full overflow-hidden sm:w-full md:w-1/3 lg:w-1/4"
+          className="my-1 px-2 w-full overflow-hidden sm:w-full md:w-1/3 lg:w-1/4 mb-8"
           key={index}
         >
-          {product.name}
+          <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <Link href={product.permalink}>
+              <a>
+                <Image
+                  layout="responsive"
+                  width="300"
+                  height="300"
+                  src={product?.images[0]?.src}
+                  className="rounded-t-lg"
+                />
+              </a>
+            </Link>
+            <div class="p-5">
+              <Link href={product.permalink}>
+                <a>
+                  <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {product.name}
+                  </h2>
+                </a>
+              </Link>
+              <Link href={product.permalink}>
+                <a class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Add to cart
+                  <svg
+                    aria-hidden="true"
+                    class="ml-2 -mr-1 w-4 h-4"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </a>
+              </Link>
+            </div>
+          </div>
         </div>
       ))}
     </div>
